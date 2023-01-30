47c38e337643f7792f58926840b07cc320d2e350
Raiymbek Merekeyev <raiymbekmerekeyev@Raiymbeks-MacBook-Pro.local>
2023-01-30 20:09:59 +0600

    Project start

---
+++ Sarqyt/SarqytApp.swift
@@ -0,0 +1,17 @@
+//
+//  SarqytApp.swift
+//  Sarqyt
+//
+//  Created by Raiymbek Merekeyev on 30.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SarqytApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
