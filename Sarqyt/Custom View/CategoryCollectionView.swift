efc5611093a2056a3ef368f2f63b14ebfe0ba9fd
Raiymbek Merekeyev <raiymbekmerekeyev@Raiymbeks-MacBook-Pro.local>
2023-02-07 12:44:14 +0600

    Creating separate view for reuse

---
+++ Sarqyt/Custom View/CategoryCollectionView.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryCollectionView.swift
+//  Sarqyt
+//
+//  Created by Raiymbek Merekeyev on 07.02.2023.
+//
+
+import Foundation
---
+++ Sarqyt/Custom View/SearchTextField.swift
@@ -0,0 +1,8 @@
+//
+//  SearchTextField.swift
+//  Sarqyt
+//
+//  Created by Raiymbek Merekeyev on 07.02.2023.
+//
+
+import Foundation
